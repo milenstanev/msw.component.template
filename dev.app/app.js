2f0f67ada1dc21cea6d13b6f6eb02a8b352c804a
Milen Stanev <milen.stanev@gmail.com>
2016-08-22 06:24:07 +0300

    Bug fix 2

--- dev.app/app.js
+++ dev.app/app.js
@@ -2,10 +2,9 @@ import angular from 'angular';
 import 'angular-ui/ui-router';
 
 //dev
-//import src from '../src/src.js';
-
+import component from '../src/component.js';
 //prod
-import component from '../src.compiled/component.js';
+//import component from '../src.compiled/component.js';
 
 export default angular.module('examlpe.core', [
   'ui.router',
